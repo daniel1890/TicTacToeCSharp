324bc673271e1347568f29a8bcbfd95da5860287
Daniël R <danielroth1890@gmail.com>
2021-02-27 16:47:57 +0100

    Creeër grid en voeg knoppen toe.

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TicTacToe
 {
@@ -20,9 +12,59 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Private Members
+
+        /// <summary>
+        /// Bewaart de huidige cellen in het actieve spel
+        /// </summary>
+        private MarkType[] mResults;
+
+        /// <summary>
+        /// Waar wanneer het de beurt van speler 1 is en false wanneer speler 2 aan de beurt is
+        /// </summary>
+        private bool mPlayer1Turn;
+
+        /// <summary>
+        /// Waar wanneer het spel beïndigd is
+        /// </summary>
+        private bool mGameEnded;
+
+        #endregion Private Members
+
+        #region Constructor
+
         public MainWindow()
         {
             InitializeComponent();
+            NewGame();
+        }
+
+        #endregion Constructor
+
+        private void NewGame()
+        {
+            // Creeër een lege array met lege cellen
+            mResults = new MarkType[9];
+
+            for (var i = 0; i < mResults.Length; i++)
+            {
+                mResults[i] = MarkType.Leeg;
+            }
+
+            // Speler 1 maakt de eerste zet
+            mPlayer1Turn = true;
+
+            // Loop door elk element wat zich bevind in het grid genaamd container....
+            Container.Children.Cast<Button>().ToList().ForEach(Button =>
+            {
+                // Verander de achtergrond, voorgrond en content naar de default waarden
+                Button.Content = string.Empty;
+                Button.Background = Brushes.White;
+                Button.Foreground = Brushes.Blue;
+            });
+
+            // Zorg ervoor dat de game niet zomaar eindigd
+            mGameEnded = false;
         }
     }
 }
---
+++ TicTacToe/MarkType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// De type staat waarin een cell zich op het moment kan bevinden.
+    /// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// De cell is nog niet geklikt
+        /// </summary>
+        Leeg,
+
+        /// <summary>
+        /// De cell is een O
+        /// </summary>
+        Rondje,
+
+        /// <summary>
+        /// De cell is een X
+        /// </summary>
+        Kruis
+    }
+}
